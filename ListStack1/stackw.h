f38e90486f6e0abbaf99d4d40445f2672b45c1ee
YingzhuWei <git@github.com>
2020-12-22 21:21:35 +0800

    first commit

---
+++ ListStack1/stackw.h
@@ -0,0 +1,23 @@
+#ifndef stackw_h
+#define stackw_h
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef int dataType;
+typedef struct node {
+	dataType data;
+	struct node *next;
+}LinkStack,node;
+
+LinkStack* InitStack();
+int Empty(LinkStack *s);
+void Push(LinkStack *s, dataType x);
+void Pop(LinkStack *s);
+dataType GetTop(LinkStack *s);
+
+int priority(char x);
+double compute(double x, double y, char op);
+
+#endif
